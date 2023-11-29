f1896b02c3c4991e3b0cc4379e7f4c1f61553677
marianofeld <marianofeld@gmail.com>
2023-11-29 13:53:29 -0300

    primer

---
+++ react/src/App.jsx
@@ -0,0 +1,18 @@
+import Productos from './components/ItemListContainer/ItemListContainer'
+import Navbar from './components/Navbar/Navbar'
+import './App.css'
+
+
+
+function App() {
+
+ return (
+  <>
+    <Navbar />
+    <Productos />
+  
+ </> 
+ )
+}
+
+export default App
